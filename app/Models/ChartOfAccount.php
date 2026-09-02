73b40d7cfc5d746296eb4e452112c7fb47ee3bc7
manwelAC <johnmanuelcuerdo@gmail.com>
2026-09-02 21:35:00 +0800

    update: chart of accounts error model

--- app/Models/ChartOfAccount.php
+++ app/Models/ChartOfAccount.php
@@ -31,4 +31,14 @@ class ChartOfAccount extends Model
     {
         return $this->belongsTo(AccountGroup::class, 'account_group_id');
     }
+
+    public function parent()
+    {
+        return $this->belongsTo(ChartOfAccount::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(ChartOfAccount::class, 'parent_id');
+    }
 }
